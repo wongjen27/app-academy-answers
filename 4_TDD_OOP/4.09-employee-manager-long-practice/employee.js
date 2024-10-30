e291d24e12913e103f2ae44328b24eb00be9494a
Jennie <wongjen27@gmail.com>
2024-10-29 17:05:52 -0700

    10/29

--- 4_TDD_OOP/4.09-employee-manager-long-practice/employee.js
+++ 4_TDD_OOP/4.09-employee-manager-long-practice/employee.js
@@ -4,12 +4,20 @@ class Employee {
         this.salary = salary;
         this.title = title;
         this.manager = manager;
+
+        if(this.manager) {
+            this.manager.addEmployee(this)
+           }
     };
+
+
 
     displayInfo() {
         console.log(`${this.name}, Title: ${this.title}, Salary: $${this.salary}, Manager: ${this.manager ? this.manager.name : 'None'}`);
     }
 
 }
+
+
 
 module.exports = Employee;
--- 4_TDD_OOP/4.09-employee-manager-long-practice/manager.js
+++ 4_TDD_OOP/4.09-employee-manager-long-practice/manager.js
@@ -1,15 +1,38 @@
 const Employee = require('./employee');
 
 class Manager extends Employee {
-    constructor(name, salary, title, manager = null, employees = []) {
+    constructor(name, salary, title, manager = null) {
         super(name, salary, title, manager);
-        this.employees = employees;
+        this.employees = [];
     }
 
     addEmployee(employee) {
         this.employees.push(employee)
     }
 
+    displayEmployees() {
+        if (this.employees.length === 0) {
+            console.log(`${this.name} manages no employees.`);
+        } else {
+            console.log(`${this.name} manages the following employees:`);
+            this.employees.forEach(emp => {
+                console.log(`- ${emp.name}, Title: ${emp.title}`);
+            });
+        }
+    }
 };
+
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+console.log('After: ', splinter);
+
+
+/****/
 
 module.exports = Manager;
