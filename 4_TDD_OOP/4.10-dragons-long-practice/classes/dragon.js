f631594260d15078ab6132041782041bfd89bfaa
Jennie <wongjen27@gmail.com>
2024-10-30 19:01:02 -0700

    10/30

--- 4_TDD_OOP/4.10-dragons-long-practice/classes/dragon.js
+++ 4_TDD_OOP/4.10-dragons-long-practice/classes/dragon.js
@@ -1,4 +1,23 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...args) {
+    let names = [];
+    args.forEach(element => {
+      names.push(element.name)
+    });
+    return names
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
