63018e5e851b745c194b69ef337f4e6a00a437ad
Jennie <wongjen27@gmail.com>
2024-10-30 19:37:04 -0700

    10/30

--- 4_TDD_OOP/4.10-dragons-long-practice/all-dragons.js
+++ 4_TDD_OOP/4.10-dragons-long-practice/all-dragons.js
@@ -1,4 +1,27 @@
 // Your code here
+const Dragon = require('./classes/dragon')
+const FriendlyDragon = require('./classes/friendly-dragon')
+const EvilDragon = require('./classes/evil-dragon')
+
+const falkor = new FriendlyDragon(
+  'Falkor',
+  'white',
+  ["save Atreyu from the swamp",
+    "save Atreyu from the Nothing",
+    "scare the local bullies into a dumpster"],
+  'Bastian')
+
+
+const smaug = new EvilDragon(
+  'Smaug',
+  'black',
+  [ "take over your mountain kingdom",
+    "steal all your dwarven gold",
+    "burn down your floating village"],
+  'Dwarf King'
+)
+
+allDragons = Dragon.getDragons(falkor,smaug)
 
 
 /****************************************************************************/
@@ -7,6 +30,7 @@
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
--- 4_TDD_OOP/4.10-dragons-long-practice/classes/evil-dragon.js
+++ 4_TDD_OOP/4.10-dragons-long-practice/classes/evil-dragon.js
@@ -1,1 +1,37 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(element => {
+            console.log(`${this.name} will ${element}`)
+        });
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+
+}
+
+const enderDragon = new EvilDragon(
+    "Ender Dragon",
+    "black", [
+      "spit a fireball at Steve",
+      "rule over all the Endermen",
+      "destroy all blocks in its way"
+    ],
+    "Steve"
+  );
+  console.log(enderDragon);
+  enderDragon.dontInviteThemOverForDinner();
+  console.log(enderDragon.breathesFire());
+  console.log(enderDragon.burnsNemesis());
+
+
+module.exports = EvilDragon
