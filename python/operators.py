7b3a306b37084fbd92ec4ed7aa05725999a30385
Jennie <wongjen27@gmail.com>
2024-11-20 14:17:04 -0800

    11/20

---
+++ python/operators.py
@@ -0,0 +1,10 @@
+#is divisible by 5
+
+
+def divisible_by_five(num):
+    return num%5==0
+
+
+print(divisible_by_five(5))    #> True
+print(divisible_by_five(-55))  #> True
+print(divisible_by_five(37))   #> False
